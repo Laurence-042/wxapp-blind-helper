1bc96ae5efe96f597d3a711758aff46af6ec7fce
Laurence042 <qq2120286535@gmail.com>
2019-11-10 09:35:09 +0800

    small fix

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 # aiohttpdemo_polls/main.py
+import pathlib
+
 from aiohttp import web
 import ssl
 from views import routes
+import sys
 
+BASE_PATH = pathlib.Path(__file__).parent.parent
+sys.path.append(BASE_PATH / "src")
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes(routes)
 
     # TODO switch the server to https mode with the code below when deploying.
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ssl_context.load_cert_chain('../ssl/2_www.laurence042.com.crt', '../ssl/3_www.laurence042.com.key')
-    web.run_app(app, host='localhost', port=9000,ssl_context=ssl_context)
+    ssl_context.load_cert_chain(BASE_PATH / 'ssl/2_www.laurence042.com.crt',
+                                BASE_PATH / 'ssl/3_www.laurence042.com.key')
+    web.run_app(app, host='localhost', port=9000, ssl_context=ssl_context)
 
     web.run_app(app)
